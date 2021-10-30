2f90da0581f3bc917b21017987730c60925f496c
Siam-Parvez <tanmoyparvez23@gmail.com>
2021-10-30 17:21:26 +0600

    some changes added

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ async function run() {
 run().catch(console.dir)
 
 app.get("/", (req, res) => {
-    res.send("Getting successfully");
+    res.send("Server is running successfully");
 });
 
 app.listen(port, () => {
